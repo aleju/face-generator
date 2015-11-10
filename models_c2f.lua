b63bbf84cbadeebe0ffbdca50713cea34c4f8683
aleju <kontakt@ajung.name>
2015-11-10 20:24:15 +0100

    Add new models

--- models_c2f.lua
+++ models_c2f.lua
@@ -10,6 +10,10 @@ local models = {}
 -- @param cuda Whether to start the network in CUDA/GPU mode (true).
 -- @returns Sequential
 function models.create_G(dimensions, cuda)
+    return models.create_G_c(dimensions, cuda)
+end
+
+function models.create_G_a(dimensions, cuda)
     local nplanes = 64
     local model_G = nn.Sequential()
     
@@ -40,13 +44,82 @@ function models.create_G(dimensions, cuda)
     return model_G
 end
 
+function models.create_G_b(dimensions, cuda)
+    local nplanes = 64
+    local model_G = nn.Sequential()
+    
+    model_G:add(nn.JoinTable(2, 2))
+    
+    if cuda then
+        model_G:add(nn.Copy('torch.FloatTensor', 'torch.CudaTensor'))
+    end
+    
+    local inner = nn.Sequential()
+    inner:add(cudnn.SpatialConvolutionUpsample(dimensions[1]+1, 64, 3, 3, 1))
+    inner:add(nn.PReLU())
+    inner:add(cudnn.SpatialConvolutionUpsample(64, 64, 3, 3, 1))
+    inner:add(nn.PReLU())
+    inner:add(cudnn.SpatialConvolutionUpsample(64, 256, 5, 5, 1))
+    inner:add(nn.PReLU())
+    inner:add(cudnn.SpatialConvolutionUpsample(256, dimensions[1], 7, 7, 1))
+    inner:add(nn.View(dimensions[1], dimensions[2], dimensions[3]))
+    model_G:add(inner)
+    if cuda then
+        model_G:add(nn.Copy('torch.CudaTensor', 'torch.FloatTensor'))
+    end
+    
+    model_G = require('weight-init')(model_G, 'heuristic')
+    
+    if cuda then
+        model_G:get(3):cuda()
+    end
+    
+    return model_G
+end
+
+function models.create_G_c(dimensions, cuda)
+    local nplanes = 64
+    local model_G = nn.Sequential()
+    
+    model_G:add(nn.JoinTable(2, 2))
+    
+    if cuda then
+        model_G:add(nn.Copy('torch.FloatTensor', 'torch.CudaTensor'))
+    end
+    
+    local inner = nn.Sequential()
+    inner:add(cudnn.SpatialConvolutionUpsample(dimensions[1]+1, 64, 3, 3, 1))
+    inner:add(nn.PReLU())
+    inner:add(cudnn.SpatialConvolutionUpsample(64, 128, 3, 3, 1))
+    inner:add(nn.PReLU())
+    inner:add(cudnn.SpatialConvolutionUpsample(128, 256, 5, 5, 1))
+    inner:add(nn.PReLU())
+    inner:add(cudnn.SpatialConvolutionUpsample(256, dimensions[1], 7, 7, 1))
+    inner:add(nn.View(dimensions[1], dimensions[2], dimensions[3]))
+    model_G:add(inner)
+    if cuda then
+        model_G:add(nn.Copy('torch.CudaTensor', 'torch.FloatTensor'))
+    end
+    
+    model_G = require('weight-init')(model_G, 'heuristic')
+    
+    if cuda then
+        model_G:get(3):cuda()
+    end
+    
+    return model_G
+end
+
 -- Create coarse to fine D network.
 -- Color and grayscale currently get the same network, which is suboptimal.
 -- @param dimensions Image dimensions as table of {count channels, height, width}
 -- @param cuda Whether to start the network in CUDA/GPU mode (true).
 -- @returns Sequential
 function models.create_D(dimensions, cuda)
-    local nplanes = 64
+    return models.create_D_c(dimensions, cuda)
+end
+
+function models.create_D_a(dimensions, cuda)
     local model_D = nn.Sequential()
     
     model_D:add(nn.CAddTable())
@@ -66,6 +139,92 @@ function models.create_D(dimensions, cuda)
     inner:add(nn.View(64 * 0.25 * dimensions[2] * dimensions[3]))
     
     inner:add(nn.Linear(64 * 0.25 * dimensions[2] * dimensions[3], 512))
+    inner:add(nn.PReLU())
+    inner:add(nn.Dropout())
+    inner:add(nn.Linear(512, 1))
+    inner:add(nn.Sigmoid())
+    model_D:add(inner)
+    if cuda then
+        model_D:add(nn.Copy('torch.CudaTensor', 'torch.FloatTensor'))
+    end
+
+    model_D = require('weight-init')(model_D, 'heuristic')
+
+    if cuda then
+        model_D:get(3):cuda()
+    end
+
+    return model_D
+end
+
+function models.create_D_b(dimensions, cuda)
+    local model_D = nn.Sequential()
+    
+    model_D:add(nn.CAddTable())
+    if cuda then
+        model_D:add(nn.Copy('torch.FloatTensor', 'torch.CudaTensor'))
+    end
+    
+    local inner = nn.Sequential()
+    
+    inner:add(nn.SpatialConvolution(dimensions[1], 64, 3, 3, 1, 1, (3-1)/2))
+    inner:add(nn.PReLU())
+    inner:add(nn.SpatialConvolution(64, 64, 3, 3, 1, 1, (3-1)/2))
+    inner:add(nn.PReLU())
+    inner:add(nn.SpatialMaxPooling(2, 2))
+    inner:add(nn.SpatialConvolution(64, 128, 3, 3, 1, 1, (3-1)/2))
+    inner:add(nn.PReLU())
+    inner:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, (3-1)/2))
+    inner:add(nn.PReLU())
+    inner:add(nn.SpatialMaxPooling(2, 2))
+    inner:add(nn.Dropout())
+    
+    inner:add(nn.View(128 * 0.25 * 0.25 * dimensions[2] * dimensions[3]))
+    
+    inner:add(nn.Linear(128 * 0.25 * 0.25 * dimensions[2] * dimensions[3], 512))
+    inner:add(nn.PReLU())
+    inner:add(nn.Dropout())
+    inner:add(nn.Linear(512, 1))
+    inner:add(nn.Sigmoid())
+    model_D:add(inner)
+    if cuda then
+        model_D:add(nn.Copy('torch.CudaTensor', 'torch.FloatTensor'))
+    end
+
+    model_D = require('weight-init')(model_D, 'heuristic')
+
+    if cuda then
+        model_D:get(3):cuda()
+    end
+
+    return model_D
+end
+
+function models.create_D_c(dimensions, cuda)
+    local model_D = nn.Sequential()
+    
+    model_D:add(nn.CAddTable())
+    if cuda then
+        model_D:add(nn.Copy('torch.FloatTensor', 'torch.CudaTensor'))
+    end
+    
+    local inner = nn.Sequential()
+    
+    inner:add(nn.SpatialConvolution(dimensions[1], 64, 3, 3, 1, 1, (3-1)/2))
+    inner:add(nn.PReLU())
+    inner:add(nn.SpatialConvolution(64, 64, 3, 3, 1, 1, (3-1)/2))
+    inner:add(nn.PReLU())
+    inner:add(nn.SpatialMaxPooling(2, 2))
+    inner:add(nn.SpatialConvolution(64, 128, 3, 3, 1, 1, (3-1)/2))
+    inner:add(nn.PReLU())
+    inner:add(nn.SpatialConvolution(128, 256, 3, 3, 1, 1, (3-1)/2))
+    inner:add(nn.PReLU())
+    inner:add(nn.SpatialMaxPooling(2, 2))
+    inner:add(nn.Dropout())
+    
+    inner:add(nn.View(256 * 0.25 * 0.25 * dimensions[2] * dimensions[3]))
+    
+    inner:add(nn.Linear(256 * 0.25 * 0.25 * dimensions[2] * dimensions[3], 512))
     inner:add(nn.PReLU())
     inner:add(nn.Dropout())
     inner:add(nn.Linear(512, 1))
