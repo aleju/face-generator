cb7242eefeacae7f8b0920c953db84270aee90d2
aleju <kontakt@ajung.name>
2015-11-19 22:26:55 +0100

    Add new architectures for G and D

--- models.lua
+++ models.lua
@@ -1,5 +1,6 @@
 require 'torch'
 require 'nn'
+require 'layers.cudnnSpatialConvolutionUpsample'
 
 local models = {}
 
@@ -19,6 +20,78 @@ function models.create_G(dimensions, noiseDim)
     return model
 end
 
+-- Creates the decoder part of an upsampling height-16px G as an autoencoder.
+-- @param dimensions The dimensions of each image as {channels, height, width}.
+-- @param noiseDim Size of the hidden layer between encoder and decoder.
+-- @returns nn.Sequential
+function models.create_G_decoder_upsampling16(dimensions, noiseDim)
+    local model = nn.Sequential()
+    model:add(nn.Linear(noiseDim, 128*4*4))
+    model:add(nn.View(128, 4, 4))
+    model:add(nn.PReLU(nil, nil, true))
+    
+    model:add(nn.SpatialUpSamplingNearest(2))
+    model:add(cudnn.SpatialConvolution(128, 256, 5, 5, 1, 1, (5-1)/2, (5-1)/2))
+    model:add(nn.SpatialBatchNormalization(256))
+    model:add(nn.PReLU(nil, nil, true))
+    
+    model:add(nn.SpatialUpSamplingNearest(2))
+    model:add(cudnn.SpatialConvolution(256, 128, 5, 5, 1, 1, (5-1)/2, (5-1)/2))
+    model:add(nn.SpatialBatchNormalization(128))
+    model:add(nn.PReLU(nil, nil, true))
+    
+    model:add(cudnn.SpatialConvolution(128, dimensions[1], 3, 3, 1, 1, (3-1)/2, (3-1)/2))
+    model:add(nn.Sigmoid())
+    
+    --model:add(nn.View(dimensions[1], dimensions[2], dimensions[3]))
+
+    model = require('weight-init')(model, 'heuristic')
+
+    return model
+end
+
+-- Creates the decoder part of an upsampling height-32px G as an autoencoder.
+-- @param dimensions The dimensions of each image as {channels, height, width}.
+-- @param noiseDim Size of the hidden layer between encoder and decoder.
+-- @returns nn.Sequential
+function models.create_G_decoder_upsampling32(dimensions, noiseDim)
+    local model = nn.Sequential()
+    model:add(nn.Linear(noiseDim, 128*8*8))
+    model:add(nn.View(128, 8, 8))
+    model:add(nn.PReLU(nil, nil, true))
+    
+    model:add(nn.SpatialUpSamplingNearest(2))
+    model:add(cudnn.SpatialConvolution(128, 256, 5, 5, 1, 1, (5-1)/2, (5-1)/2))
+    model:add(nn.SpatialBatchNormalization(256))
+    model:add(nn.PReLU(nil, nil, true))
+    
+    model:add(nn.SpatialUpSamplingNearest(2))
+    model:add(cudnn.SpatialConvolution(256, 128, 5, 5, 1, 1, (5-1)/2, (5-1)/2))
+    model:add(nn.SpatialBatchNormalization(128))
+    model:add(nn.PReLU(nil, nil, true))
+    
+    model:add(cudnn.SpatialConvolution(128, dimensions[1], 3, 3, 1, 1, (3-1)/2, (3-1)/2))
+    model:add(nn.Sigmoid())
+    
+    --model:add(nn.View(dimensions[1], dimensions[2], dimensions[3]))
+
+    model = require('weight-init')(model, 'heuristic')
+
+    return model
+end
+
+-- Creates G, which is identical to the decoder part of G as an autoencoder.
+-- @param dimensions The dimensions of each image as {channels, height, width}.
+-- @param noiseDim Size of the hidden layer between encoder and decoder.
+-- @returns nn.Sequential
+function models.create_G(dimensions, noiseDim)
+    if dimensions[2] == 16 then
+        return models.create_G_decoder_upsampling16(dimensions, noiseDim)
+    else
+        return models.create_G_decoder_upsampling32(dimensions, noiseDim)
+    end
+end
+
 -- Create base D network.
 -- @param dimensions Image dimensions as table of {count channels, height, width}
 -- @returns Sequential
@@ -26,7 +99,7 @@ function models.create_D(dimensions)
     if dimensions[2] == 16 then
         return models.create_D16_d(dimensions)
     else
-        return models.create_D32(dimensions)
+        return models.create_D32b(dimensions)
     end
 end
 
@@ -300,6 +373,46 @@ function models.create_D32(dimensions)
     model:add(nn.Sigmoid())
 
     return model
+end
+
+-- Create base D network for 32x32 images.
+-- Color and grayscale currently get the same network, which is suboptimal.
+-- @param dimensions Image dimensions as table of {count channels, height, width}
+-- @returns Sequential
+function models.create_D32b(dimensions)
+    local conv = nn.Sequential()
+    
+    conv:add(nn.SpatialConvolution(dimensions[1], 64, 3, 3, 1, 1, (3-1)/2))
+    conv:add(nn.PReLU(nil, nil, true))
+    conv:add(nn.SpatialDropout(0.2))
+    conv:add(nn.SpatialAveragePooling(2, 2, 2, 2))
+    
+    conv:add(nn.SpatialConvolution(64, 128, 3, 3, 1, 1, (3-1)/2))
+    conv:add(nn.PReLU(nil, nil, true))
+    conv:add(nn.SpatialDropout(0.2))
+    conv:add(nn.SpatialAveragePooling(2, 2, 2, 2))
+    
+    conv:add(nn.SpatialConvolution(128, 256, 3, 3, 1, 1, (3-1)/2))
+    conv:add(nn.PReLU(nil, nil, true))
+    conv:add(nn.SpatialDropout(0.2))
+    conv:add(nn.SpatialAveragePooling(2, 2, 2, 2))
+    
+    conv:add(nn.SpatialConvolution(256, 512, 3, 3, 1, 1, (3-1)/2))
+    conv:add(nn.PReLU(nil, nil, true))
+    conv:add(nn.SpatialDropout(0.2))
+    conv:add(nn.SpatialAveragePooling(2, 2, 2, 2))
+    
+    conv:add(nn.View(512 * 0.25 * 0.25 * 0.25 * 0.25 * dimensions[2] * dimensions[3]))
+    conv:add(nn.Linear(512 * 0.25 * 0.25 * 0.25 * 0.25 * dimensions[2] * dimensions[3], 512))
+    conv:add(nn.PReLU(nil, nil, true))
+    conv:add(nn.Dropout())
+    conv:add(nn.Linear(512, 512))
+    conv:add(nn.PReLU(nil, nil, true))
+    conv:add(nn.Dropout())
+    conv:add(nn.Linear(512, 1))
+    conv:add(nn.Sigmoid())
+
+    return conv
 end
 
 return models
